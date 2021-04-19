e2a9c4bbe12989500ab9788b65f9ccd0bd8e16dd
Ramir Sultanov <ramirrsultanov@stud.kpfu.ru>
2021-04-19 09:58:23 +0300

    commit 1

--- mainwindow.h
+++ mainwindow.h
@@ -4,18 +4,49 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui
+{
+class MainWindow;
+}
 QT_END_NAMESPACE
+
+class Price
+{
+public:
+  virtual int calculatePrice(std::string from, std::string transport, std::string to);
+};
+
+class PriceError
+{
+public:
+  virtual QString getPriceError(int id);
+};
+
+class Result
+{
+public:
+  virtual QString getResult(QString transport);
+};
+
+class Delivery : public Price, public PriceError, public Result
+{
+};
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+  MainWindow(QWidget* parent = nullptr);
+  ~MainWindow();
+
+private slots:
+  void on_calculate_button_clicked();
+
+  void on_freight_button_clicked();
 
 private:
-    Ui::MainWindow *ui;
+  Ui::MainWindow* ui;
+  Delivery delivery;
 };
-#endif // MAINWINDOW_H
+#endif  // MAINWINDOW_H
